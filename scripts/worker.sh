629c2f0936813a9978b74c0ebefe75694cee329d
Gustavo Oliveira Hoyer <gustavo.hoyer@gmail.com>
2022-06-07 21:18:36 -0300

    fix(worker): Corrige o comando de instalacao

--- scripts/worker.sh
+++ scripts/worker.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
-
+export DEBIAN_FRONTEND=noninteractive
+echo "###############################"
+echo "## CONFIGURANDO O K3S WORKER ##"
+echo "###############################"
 TOKEN=$(echo Token super secreto do Lab | base64)
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.11:6443 K3S_TOKEN=$TOKEN sh -
-yum install -y iscsi-initiator-utils.x86_64 libiscsi.x86_64 libiscsi-utils.x86_64 nfs-utils.x86_64
+curl -sfL https://get.k3s.io | K3S_URL=https://master-1:6443 K3S_TOKEN=$TOKEN sh -
+apt-get -y install open-iscsi libiscsi7 libiscsi-bin nfs-common
