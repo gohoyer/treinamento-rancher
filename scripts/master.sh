93dbe3b6703d6af47e84bb4b753c5953de782cb2
Gustavo Oliveira Hoyer <gustavo.hoyer@gmail.com>
2022-06-07 21:18:05 -0300

    fix(master): Acrescenta a variavel INSTALL_K3S_EXEC

--- scripts/master.sh
+++ scripts/master.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
-
+export DEBIAN_FRONTEND=noninteractive
+echo "###############################"
+echo "## CONFIGURANDO O K3S MASTER ##"
+echo "###############################"
 TOKEN=$(echo Token super secreto do Lab | base64)
+# From https://superuser.com/questions/1670418/k3s-slave-agents-dont-join-kubernetes-cluster-in-vagrantbox-setup
+masterIP="192.168.56.11"
+flags="--tls-san $masterIP --node-external-ip $masterIP"
 curl -sfL https://get.k3s.io | \
 K3S_TOKEN=$TOKEN \
+INSTALL_K3S_EXEC="$flags" \
 sh -s - server \
 --node-taint CriticalAddonsOnly=true:NoExecute
 kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
